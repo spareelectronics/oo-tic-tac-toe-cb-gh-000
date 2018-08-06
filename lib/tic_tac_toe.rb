d93acfd25eda1176f1957e0a59a201bc58203c36
Amin Ullah <aullah@umich.edu>
2018-08-06 23:19:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,9 +24,29 @@ def display_board
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+def input_to_index
+  @user_input.to_i - 1
+end
 
+def move
+  @board[@index] = @current_player
+end
 
+def valid_move?
+  @index.between?(0,8) && !position_taken?(@board, @index)
+end
 
+def turn
+  puts "Please enter 1-9:"
+  @input = gets.strip
+  @index = input_to_index(input)
+  if valid_move?(@board, @index)
+    move(@board, @index, current_player(@board))
+    display_board(@board)
+  else
+    turn(@board)
+  end
+end
 
 
 
