07a8c3b0d69461380fe6a07d53e7f567bf089870
Amin Ullah <aullah@umich.edu>
2018-08-06 23:54:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ def turn
   input = gets.strip
   @index = input_to_index(input)
   if valid_move?(@index)
-    move(index,current_player)
+    move(@index,current_player)
     display_board
   else
     turn
